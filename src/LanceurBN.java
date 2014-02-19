1533cddb6ef4c59f744be59101c586e703a9a68c
Vervacke <valentin.vervacke@iut-valence.fr>
2014-02-19 16:59:44 +0100

    Compilation Départ Début création grille --> Associer Boolean TRUE a bateau et FALSE a vide

---
+++ src/LanceurBN.java
@@ -0,0 +1,23 @@
+/** Imports placés dans le package fr.iutvalence.java.tp.BN **/
+import fr.iutvalence.java.tp.BN.BatailleNavale;
+
+/**
+ * Bataille Navale
+ * Contient le "main" qui permet le démarage du jeu
+ */
+public class LanceurBN
+{ 
+	/**
+	 * Initialisation du jeu
+	 * Création d'une partie suivi du démarrage de celle-ci
+	 * @param args 
+	 */
+	public static void main(String[] args)
+		{
+		System.out.println ("Création de la partie");
+		BatailleNavale partieDeBatailleNavale = new BatailleNavale();
+		System.out.println("Demarrage de la partie");
+		partieDeBatailleNavale.jouer();
+		System.out.println("Fin de la partie");
+	}
+}
---
+++ src/fr/iutvalence/java/tp/BN/BatailleNavale.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package fr.iutvalence.java.tp.BN;
+import java.awt.Frame;
+import java.awt.GridLayout;
+
+import javax.sound.sampled.BooleanControl;
+
+/**
+ * @author vervackv
+ *
+ */
+public class BatailleNavale
+{
+	public static final Boolean 
+	/**
+	 * 
+	 */
+	public BatailleNavale()
+	{
+		boolean Grille[][];
+				{
+				
+				}
+	}
+	
+	/**
+	 * 
+	 */
+	public void jouer()
+	{
+		
+	}
+
+}
